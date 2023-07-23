e408b4bded9e20ca9af3470a854d148f46a6b712
Amber Taveras <taveras.amber@gmail.com>
2023-07-23 01:11:57 -0400

    finalizes big O section notes

--- big-o.js
+++ big-o.js
@@ -1,3 +1,6 @@
+// Time Complexity
+
+
 // write a function that calculates the sum of all numbers from 1 up to and including some number n
 
 function addUpTo(n) {
@@ -8,6 +11,8 @@ function addUpTo(n) {
     return total;
 }
 // number of operations are depending on n, growing proportionally with n
+// O(n) -- bounded by multiple of n, time
+// O(1) space, doesn't create more variables/data for each input
 
 
 
@@ -15,6 +20,7 @@ function addUpTo(n) {
     return n * (n + 1) / 2;
 }
 // 3 simple operations regardless of n
+// O(1) -- linear run time
 
 
 
@@ -31,8 +37,58 @@ which code is better?
 let t1 = performance.now();
 addUpTo(1000000000);
 let t2 = performance.now();
-console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`)
+// console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`)
     // not very reliable --> varies alot across runtimes even on same machine
 
 
 // rather than counting seconds, count the number of simple operations the computer has to perform
+
+
+function countUpAndDown (n) {
+    console.log('Going up!');
+    for (let i = 0; i < n; i++) {
+        console.log(i);
+    }
+    console.log('At the top!');
+    console.log('Going down...');
+    for (let j = n; j >= 0; j--) {
+        console.log(j);
+    }
+    console.log('Back down. Bye!');
+}
+// both loops are O(n), overall the function is still considered O(n) - time
+
+
+function printAllPairs (n) {
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            console.log(i, j);
+        }
+    }
+}
+// O(n^2), quadratic time
+
+
+function logAtLeast5 (n) {
+    for (let i = 1; i <= Math.max(5, n); i++) {
+        console.log(i);
+    }
+}
+// O(n) - time
+
+function logAtMost5 (n) {
+    for (let i = 1; i <= Math.min(5, n); i++) {
+        console.log(i);
+    }
+}
+// O(1) - time
+
+
+function double(arr) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        newArr.push(2 * arr[i]);
+    }
+}
+// array is getting longer directly porportionate to the input n
+// O(n) space
