80578cc0c1f35aad0e0fd03ca973a290e571d9c3
Amber Taveras <taveras.amber@gmail.com>
2023-08-28 23:38:08 -0400

    adds get, set, insert, remove, reverse s linked list methods

--- data-structures/singly-linked-list.js
+++ data-structures/singly-linked-list.js
@@ -69,10 +69,64 @@ class SinglyLinkedList {
         let currentHead = this.head;
         this.head = currentHead.next
         this.length--;
-        if (this.length === 0) {
-            this.tail = null;
-        }
+        if (this.length === 0) this.tail = null;
         return currentHead;
+    }
+    get (index) {
+        if (index < 0 || index >= this.length) return null;
+        let counter = 0;
+        let current = this.head;
+        while (counter !== index) {
+            current = current.next;
+            counter++;
+        }
+        return current;
+    }
+    set (index, val) {
+        let foundNode = this.get(index);
+        if (foundNode) {
+            foundNode.val = val;
+            return true;
+        }
+        return false;
+    }
+    insert (index, val) {
+        if (index < 0 || index > this.length) return false;
+        if (index === this.length) return !!this.push(val); // double bang to coerce boolean
+        if (index === 0) return !!this.unshift(val); // double bang to coerce boolean
+        let newNode = new Node(val);
+        let previous = this.get(index - 1);
+        let temp = previous.next;
+        previous.next = newNode;
+        newNode.next = temp;
+        this.length++;
+        return true;
+    }
+    remove (index) {
+        if (index < 0 || index >= this.length) return undefined;
+        if (index === 0) return this.shift();
+        if (index === this.length - 1) return this.pop();
+        let previousNode = this.get(index - 1);
+        let removed = previousNode.next;
+        previousNode.next = removed.next;
+        this.length--;
+        return removed;
+    }
+    reverse () {
+        // swap head and tail
+        let node = this.head;
+        this.head = this.tail;
+        this.tail = node;
+
+        // iterate through and swap values
+        let next;
+        let previous = null;
+        for (let i = 0; i < this.length; i++) {
+            next = node.next;
+            node.next = previous;
+            previous = node;
+            node = next;
+        }
     }
 }
 
@@ -82,7 +136,17 @@ list.push('goodbye')
 list.push('good')
 list.push('morning')
 list.push('goodnight')
-// list.traverse();
+
+
+
 // list.shift();
 // list.unshift('hi');
+// (list.get(3);
+// list.set(2, 'testing')
+// list.insert(4, 'something')
+// list.remove(2)
+// list.reverse();
+
+
+// list.traverse();
 // console.log(list);
