438b06f5ba8f0bd9c97d447484d8096733493361
Amber Taveras <taveras.amber@gmail.com>
2023-10-03 00:07:07 -0400

    implements push and pop exercise

---
+++ data-structures/SLL-exercises.js
@@ -0,0 +1,51 @@
+class Node {
+    constructor (val) {
+        this.val = val;
+        this.next = null;      
+    }
+}
+
+class SinglyLinkedList{
+
+    constructor (val) {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    // implement push
+    push (val) {
+        let newNode = new Node(val);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+    // implement pop
+    pop () {
+        if (!this.head) return undefined;
+        let current = this.head;
+        let newTail = current;
+        while (current.next) {
+            newTail = current;
+            current = current.next;
+        }
+        this.tail = newTail;
+        this.tail.next = null;
+        this.length--;
+        if (this.length === 0) {
+            this.head = null;
+            this.tail = null;
+        }
+        return current;
+    }
+    // implement get
+    get () {
+        
+    }
+}
+
