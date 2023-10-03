27e2117b225da704ac86f7f7eaad9666e898fa4c
Amber Taveras <taveras.amber@gmail.com>
2023-10-03 01:42:21 -0400

    sets up doubly linked list and node classes

---
+++ data-structures/doubly-linked-list.js
@@ -0,0 +1,19 @@
+// piece of data --> val
+// reference to the next node --> next
+// reference to the previous node --> prev
+
+class Node {
+    constructor (val) {
+        this.val = val;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class DoublyLinkedList {
+    constructor () {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+}
