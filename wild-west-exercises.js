782652be7427f7e1feec16146c317d9db3729cce
Amber Taveras <taveras.amber@gmail.com>
2023-10-13 22:39:31 -0400

    counts zeros in binary array, ex 41

--- wild-west-exercises.js
+++ wild-west-exercises.js
@@ -1,15 +1,39 @@
 // exercise 41, divide and conquer - countZeroes
+function countZeroes (arr) {
+
+    let firstIndex = binarySearch(arr);
+
+    if (firstIndex === -1) return 0;
+    else return arr.length - firstIndex;
+
+    function binarySearch (arr) {
+        let left = 0;
+        let right = arr.length - 1;
+    
+        let index = -1;
+    
+        while (left <= right) {
+            let midpoint = Math.round((right + left) / 2);
+            if (arr[midpoint] === 0) {
+                index = midpoint;
+                right = midpoint - 1;
+            } else if (arr[midpoint] > 0) left = midpoint + 1;
+            else right = midpoint - 1;
+        }
+        return index;
+    }
+}
 
 // exercise 42, divide and conquer - sortedFrequency
 function sortedFrequency (arr, val) {
-    let firstIndex = binarySearch(arr, val, true);
+    let firstIndex = binarySearchForIndex(arr, val, true);
     if (firstIndex === -1) return -1;
-    let lastIndex = binarySearch(arr, val, false);
+    let lastIndex = binarySearchForIndex(arr, val, false);
 
     return lastIndex - firstIndex + 1;
 }
 
-function binarySearch (arr, val, isFirst) {
+function binarySearchForIndex (arr, val, isFirst) {
     let left = 0;
     let right = arr.length - 1;
 
