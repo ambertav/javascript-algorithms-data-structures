c1e6e45ba921accaa93eec7a7cd47dc2479c487c
Amber Taveras <taveras.amber@gmail.com>
2023-10-16 01:10:06 -0400

    implements insertion sort

--- wild-west-exercises.js
+++ wild-west-exercises.js
@@ -121,7 +121,7 @@ function bubbleSort(arr, comparator) {
 
 // exercise 45, selection sort
 function selectionSort (arr, comparator) {
-    if (typeof comparator !== 'function') return selectionSort(arr, compareNumbers)
+    if (typeof comparator !== 'function') return selectionSort(arr, compareNumbers);
     for (let i = 0; i < arr.length; i++) {
         let min = i;
         for (let j = i + 1; j < arr.length; j++) {
@@ -196,4 +196,20 @@ class SinglyLinkedList {
         }
         return removedNode;
     }
+}
+
+
+// exercise 47, insertion sort
+function insertionSort (arr, comparator) {
+    if (typeof comparator !== 'function') return insertionSort(arr, compareNumbers);
+    for (let i = 1; i < arr.length; i++) {
+        let current = arr[i];
+        let j = i - 1;
+        while (j >= 0 && comparator(arr[j], current) > 0) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = current;
+    }
+    return arr;
 }
