96083146dea38d62b77d3d10ef2e15d6f833aea4
Amber Taveras <taveras.amber@gmail.com>
2024-02-16 14:08:16 -0500

    completes w graph and dijkstras exercise

--- wild-west-exercises.js
+++ wild-west-exercises.js
@@ -392,7 +392,7 @@ class Stack {
     pop() {
         // if no nodes in first queue, return null
         if (!this.queueOne.first) return null;
-        
+
         // else return the dequeue of first queue
         else return this.queueOne.dequeue().value;
     }
@@ -785,6 +785,120 @@ class Graph {
 }
 
 // exercise 75, dijkstra exercise
+function WeightedGraph () {
+    /*** Use Graph as a constructor for WeightedGraph to inherit from! ***/
+    Graph.call(this);
+}
+
+WeightedGraph.prototype = Object.create(Graph.prototype);
+WeightedGraph.prototype.constructor = WeightedGraph;
+
+WeightedGraph.prototype.addEdge = function (vertex1, vertex2, weight) {
+    this.adjacencyList[vertex1].push({ node: vertex2, weight });
+    this.adjacencyList[vertex2].push({ node: vertex1, weight });
+}
+
+WeightedGraph.prototype.dijkstra = function (start, finish) {
+    const nodes = new PriorityQueue();
+    const distances = {};
+    const previous = {};
+
+    let path = [];
+    let smallest;
+
+    for (let vertex in this.adjacencyList) {
+        if (vertex === start) {
+            distances[vertex] = 0;
+            nodes.enqueue(vertex, 0);
+        } else {
+            distances[vertex] = Infinity;
+            nodes.enqueue(vertex, Infinity);
+        }
+        previous[vertex] = null;
+    }
+
+    while (nodes.values.length) {
+        smallest = nodes.dequeue().val;
+        if (smallest === finish) {
+            while(previous[smallest]) {
+                path.push(smallest);
+                smallest = previous[smallest];
+            }
+            break;
+        }
+        if (smallest || distances[smallest] !== Infinity) {
+            for (let neighbor in this.adjacencyList[smallest]) {
+                let nextNode = this.adjacencyList[smallest][neighbor];
+                let candidate = distances[smallest] + nextNode.weight;
+                if (candidate < distances[nextNode.node]) {
+                    distances[nextNode.node] = candidate;
+                    previous[nextNode.node] = smallest;
+                    nodes.enqueue(nextNode.node, candidate);
+                }
+            }
+        }
+    }
+    return path.concat(smallest).reverse();
+}
+
+function Graph () {
+  this.adjacencyList = {};
+}
+
+Graph.prototype.numEdges = function () {
+  let total = 0;
+
+  Object.values(this.adjacencyList).forEach(list => {
+    total += list.length;
+  });
+
+  // note that we've double-counted up til now since we've looked at
+  // the adjacencyList for every node.
+  return total / 2;
+};
+
+Graph.prototype.addVertex = function (vertex) {
+  this.adjacencyList[vertex] = [];
+};
+
+Graph.prototype.addEdge = function (vertex1, vertex2) {
+  this.adjacencyList[vertex1].push(vertex2);
+  this.adjacencyList[vertex2].push(vertex1);
+};
+
+Graph.prototype.removeVertex = function (vertex) {
+  while (this.adjacencyList[vertex].length) {
+    const adjacentVertex = this.adjacencyList[vertex].pop();
+    this.removeEdge(adjacentVertex, vertex);
+  }
+  delete this.adjacencyList[vertex];
+};
+
+Graph.prototype.removeEdge = function (vertex1, vertex2) {
+  this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+    v => v !== vertex2
+  );
+  this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+    v => v !== vertex1
+  );
+};
+
+/***  Use the following as a PriorityQueue (it's a min heap)! ***/
+class PriorityQueue {
+  constructor () {
+    this.values = [];
+  }
+  enqueue (val, priority) {
+    this.values.push({val, priority });
+    this.sort();
+  }
+  dequeue () {
+    return this.values.shift();
+  }
+  sort () {
+    this.values.sort((a, b) => a.priority - b.priority);
+  };
+}
 
 // exercise 76, dynamic programming coin change
 
